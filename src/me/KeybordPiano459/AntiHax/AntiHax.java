8c528f425aaeebd9cecff2578844cbae72ae464e
KeybordPiano459 <jbcook123@gmail.com>
2012-10-08 19:49:37 -0400

    Added Metrics

--- src/me/KeybordPiano459/AntiHax/AntiHax.java
+++ src/me/KeybordPiano459/AntiHax/AntiHax.java
@@ -1,11 +1,24 @@
 package me.KeybordPiano459.AntiHax;
 
+import java.io.IOException;
+
+import me.KeybordPiano459.AntiHax.util.Metrics;
+
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AntiHax extends JavaPlugin {
 	public void onEnable() {
 		getLogger().info("AntiHax 1.0 has been enabled!");
+		
+		registerEvents();
+		
+		try {
+		    Metrics metrics = new Metrics(this);
+		    metrics.start();
+		} catch (IOException e) {
+		    // Failed to submit stats :-(
+		}
 	}
 	
 	public void onDisable() {
