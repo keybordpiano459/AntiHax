7204d79fff115acd1a33c1c96987fd8faa505cab
KeybordPiano459 <jbcook123@gmail.com>
2012-10-09 18:59:19 -0400

    Added some new checks

--- src/me/KeybordPiano459/AntiHax/AntiHax.java
+++ src/me/KeybordPiano459/AntiHax/AntiHax.java
@@ -1,7 +1,15 @@
 package me.KeybordPiano459.AntiHax;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import me.KeybordPiano459.AntiHax.checks.blockevents.Reach;
+import me.KeybordPiano459.AntiHax.checks.movement.SprintNoFood;
+import me.KeybordPiano459.AntiHax.checks.movement.WalkOnWater;
 import me.KeybordPiano459.AntiHax.util.Metrics;
 import me.KeybordPiano459.AntiHax.util.UpdateEvent;
 import me.KeybordPiano459.AntiHax.util.UpdateNotifier;
@@ -14,14 +22,14 @@ public class AntiHax extends JavaPlugin {
 	
 	public static boolean update;
 	public int version = Integer.parseInt(Bukkit.getServer().getPluginManager().getPlugin("AntiHax").getDescription().getVersion());
+	private Date now = new Date();
+	private SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 	
 	public void onEnable() {
 		getLogger().info("AntiHax " + version + " has been enabled!");
 		
 		registerEvents();
 		UpdateNotifier.updateNotifier();
-		getConfig().options().copyDefaults(true);
-	    saveConfig();
 		
 		try {
 		    Metrics metrics = new Metrics(this);
@@ -37,10 +45,77 @@ public class AntiHax extends JavaPlugin {
 	
 	public void registerEvents() {
 		PluginManager pm = getServer().getPluginManager();
+		//Actions
+		pm.registerEvents(new Actions(this), this);
+		pm.registerEvents(new PlayerLogin(this), this);
+		
+		//Block Event Checks
 		pm.registerEvents(new Reach(this), this);
 		
-		if (update) {
-			pm.registerEvents(new UpdateEvent(), this);
+		//Movement Checks
+		pm.registerEvents(new WalkOnWater(this), this);
+		pm.registerEvents(new SprintNoFood(this), this);
+		
+		if(update){pm.registerEvents(new UpdateEvent(),this);}
+	}
+	
+	public void logCheat(String message) {
+		try {
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) {
+            	dataFolder.mkdir();
+			}
+			File saveTo = new File(getDataFolder(), "hack.txt");
+			if (!saveTo.exists()) {
+				saveTo.createNewFile();
+			}
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("[" + format.format(now) + "] " + message);
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void logAction(String message) {
+		try {
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) {
+            	dataFolder.mkdir();
+			}
+			File saveTo = new File(getDataFolder(), "action.txt");
+			if (!saveTo.exists()) {
+				saveTo.createNewFile();
+			}
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("[" + format.format(now) + "] " + message);
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void logPlayerLogin(String message) {
+		try {
+			File dataFolder = getDataFolder();
+			if(!dataFolder.exists()) {
+            	dataFolder.mkdir();
+			}
+			File saveTo = new File(getDataFolder(), "login.txt");
+			if (!saveTo.exists()) {
+				saveTo.createNewFile();
+			}
+			FileWriter fw = new FileWriter(saveTo, true);
+			PrintWriter pw = new PrintWriter(fw);
+			pw.println("[" + format.format(now) + "] " + message);
+			pw.flush();
+			pw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
