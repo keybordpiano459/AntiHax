71b6edaa3eea95b8760e62f18b18a1bc3b1250f9
KeybordPiano459 <jbcook123@gmail.com>
2012-10-20 19:22:08 -0400

    Added Hit Self Check

---
+++ src/me/KeybordPiano459/AntiHax/checks/fight/HitSelf.java
@@ -0,0 +1,31 @@
+package me.KeybordPiano459.AntiHax.checks.fight;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+import me.KeybordPiano459.AntiHax.AntiHax;
+import me.KeybordPiano459.AntiHax.checks.Check;
+
+public class HitSelf extends Check implements Listener {
+	AntiHax plugin;
+    public HitSelf(AntiHax plugin) {
+        this.plugin = plugin;
+    }
+	@EventHandler (priority = EventPriority.HIGHEST)
+	public void onEntityDmg(EntityDamageByEntityEvent event) {
+		Player player = (Player) event.getEntity();
+		Entity attacker = event.getDamager();
+		Entity victim = event.getEntity();
+		if (!player.hasPermission("antihax.check.hitself")) {
+			if (attacker == player && victim == attacker) {
+				event.setCancelled(true);
+				PlayerKick(player, plugin, "Don't Hit Yourself!", "tried to hit himself");
+				AntiHax.hitself++;
+			}
+		}
+	}
+}
